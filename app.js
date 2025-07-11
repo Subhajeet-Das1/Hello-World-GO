94117f39bb222932d1aecbd9be6a6bad14bbf05f
Subhajeet-Das1 <subhajeet620@gmail.com>
2025-07-11 12:42:08 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World from Node.js!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
